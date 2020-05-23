b32c5691c0a51af177a2560e3078b571c1ec6fe7
Niels Jensen <niels@hjouztljghfguzge.localdomain>
2020-05-23 14:11:41 +0200

    Updates

---
+++ precache-manifest.91a8af9ca9b68a4b92a5cd660728e249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa516ef6e4051b2dad6e25ef63f7214",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e8ac744694d681982bc5",
+    "url": "/monsters-rolodex/static/css/main.7efa9621.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ac744694d681982bc5",
+    "url": "/monsters-rolodex/static/js/main.96d31be3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
